3eecb10f54dfe6c78d6a1ed0b3619c26858e24bd
Bebop <nneven@usc.edu>
2023-12-17 20:10:35 -0500

    Create page-v1.tsx

---
+++ src/app/blackrock-robotics-and-ai-etf/page-v1.tsx
@@ -0,0 +1,104 @@
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { CalendarDateRangePicker } from "@/components/examples/date-range-picker";
+import { MainNav } from "@/components/examples/main-nav";
+import { Search } from "@/components/examples/search";
+import TeamSwitcher from "@/components/examples/team-switcher";
+import { UserNav } from "@/components/examples/user-nav";
+// import { Overview } from "@/components/examples/overview";
+import { RecentSales } from "@/components/examples/recent-sales";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Example dashboard app built using the components.",
+};
+
+export default function DashboardPage() {
+  return (
+    <>
+      <div className="md:hidden">
+        <Image
+          src="/examples/dashboard-light.png"
+          width={1280}
+          height={866}
+          alt="Dashboard"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/examples/dashboard-dark.png"
+          width={1280}
+          height={866}
+          alt="Dashboard"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="hidden flex-col md:flex">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+            <TeamSwitcher />
+            <MainNav className="mx-6" />
+            <div className="ml-auto flex items-center space-x-4">
+              <Search />
+              <UserNav />
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <div className="flex items-center justify-between space-y-2">
+            <h2 className="text-3xl font-bold tracking-tight">
+              BlackRock Robotics and AI Multisector ETF
+            </h2>
+            <div className="flex items-center space-x-2">
+              <CalendarDateRangePicker />
+              <Button>Download</Button>
+            </div>
+          </div>
+          <Tabs defaultValue="overview" className="space-y-4">
+            {/* <TabsList>
+              <TabsTrigger value="overview">Overview</TabsTrigger>
+              <TabsTrigger value="analytics">Analytics</TabsTrigger>
+              <TabsTrigger value="reports">Reports</TabsTrigger>
+              <TabsTrigger value="notifications">Notifications</TabsTrigger>
+            </TabsList> */}
+            <TabsContent value="overview" className="space-y-4">
+              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-8">
+                <Card className="col-span-5 h-[120vh]">
+                  <CardHeader>
+                    <CardTitle>Report</CardTitle>
+                  </CardHeader>
+                  <CardContent className="h-[calc(100%-4rem)]">
+                    <iframe
+                      className="w-full h-full"
+                      src="blackrock_report_jeff_demo.pdf"
+                    />
+                  </CardContent>
+                </Card>
+                <Card className="col-span-3">
+                  <CardHeader>
+                    <CardTitle>Data Sources</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <RecentSales />
+                  </CardContent>
+                </Card>
+              </div>
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+    </>
+  );
+}
